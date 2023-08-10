817139a72d9c58342ee865af0f6b6b8d72951a63
luufigueroa8 <141746156+luufigueroa8@users.noreply.github.com>
2023-08-10 10:51:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function galletas() {
+    // genera los carteles
+    alert("Bienvenido")
+    // cartel para aceptar
+    
+    confirm("Sos humano?")
+    // cartel de aceptar o cancelar
+    
+    if (confirm("Querés galletas?")) {
+        alert("Provecho")
+        // esto se ejecuta si es true
+        prompt("Te gustaron?")
+        // cuadro de pregunta
+    } else{
+        alert("Más para mi")
+        // esto se ejecuta si es falso
+    }    
+    
+}
+
+
+
